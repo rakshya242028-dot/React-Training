c26e0a1096e49afb21a9e9205ef1c5ebc4473ce3
rakshya242028 <rakshya.242028@gmail.com>
2025-12-23 07:07:27 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const [todos, setTodos] = useState([
+    
+  ]);
+
+  const addTodo = () => {
+    if (input.trim() !== "") {
+      setTodos([...todos, { id: Date.now(), value: input }]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <br />
+      <br />
+
+      <button onClick={addTodo}>Add</button>
+
+      <ul style={{ listStylePosition: "inside", padding: 0 }}>
+        {todos.map((todoItem) => (
+          <li key={todoItem.id}>{todoItem.value}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
